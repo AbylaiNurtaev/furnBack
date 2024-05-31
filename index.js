6b15c7fc2ff6159e2c89f90f4601ce2c5b65c187
ITP Project <krutyev6@gmail.com>
2024-06-01 01:53:34 +0600

    dasdasdsa

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.patch('/posts/:title', checkAdmin, postCreateValidation, PostController.upda
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4444, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
