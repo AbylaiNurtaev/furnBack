250bb78f8b5b2dd9d737e97a49cc9876e52842a8
ITP Project <krutyev6@gmail.com>
2024-06-02 13:45:22 +0600

    dinner

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,16 +1,13 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { validationResult } from 'express-validator'
-
-import UserModel from './../models/User.js'
+import argon2 from 'argon2';
+import { validationResult } from 'express-validator';
+import UserModel from './../models/User.js';
 
 export const register = async (req, res) => {
-    try{
-    
+    try {
         const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-        
+        const hash = await argon2.hash(password);
+
         const doc = new UserModel({
             email: req.body.email,
             fullName: req.body.fullName,
@@ -19,25 +16,23 @@ export const register = async (req, res) => {
             favouriteItems: []
         });
 
-        const user = await doc.save()
+        const user = await doc.save();
 
         const token = jwt.sign({
             _id: user._id
-        }, 'secret123',
-        {
+        }, 'secret123', {
             expiresIn: "30d",
-        },
-    )
-    
-    const {passwordHash, ...userData} = user._doc
+        });
+
+        const { passwordHash, ...userData } = user._doc;
         res.json({
             ...userData,
             token,
-        })
-    } catch(err){
-        console.log(err)
+        });
+    } catch (err) {
+        console.log(err);
         res.status(500).json({
-            message: "не удалось зарегестрироваться",
+            message: "Не удалось зарегистрироваться",
         });
     }
 }
@@ -46,43 +41,37 @@ export const login = async (req, res) => {
     try {
         const user = await UserModel.findOne({ email: req.body.email });
 
-        if(!user){
+        if (!user) {
             return res.status(404).json({
                 message: "Пользователь не найден"
-            })
+            });
         }
 
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+        const isValidPass = await argon2.verify(user._doc.passwordHash, req.body.password);
 
-        if(!isValidPass){
-            if(!isValidPass){
-                return res.status(400).json({
-                    message: "Неверный логин или пароль"
-                })
-            }
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: "Неверный логин или пароль"
+            });
         }
 
-        const token = jwt.sign(
-        {
-            _id: user._id, 
-        },
-        'secret123',
-        {
+        const token = jwt.sign({
+            _id: user._id,
+        }, 'secret123', {
             expiresIn: "30d",
-        },
-    );
+        });
 
-    const { passwordHash, ...userData } = user._doc;
-    res.json({
-        ...userData,
-        token,
-    })
+        const { passwordHash, ...userData } = user._doc;
+        res.json({
+            ...userData,
+            token,
+        });
 
     } catch (err) {
         console.log(err);
         res.status(500).json({
             message: "Не удалось авторизоваться"
-        })
+        });
     }
 }
 
